27fff0298231d3dc6aac5012e3667dfc1c182f67
Mango T14 <kubajaskow@wp.pl>
2024-06-03 23:26:52 +0200

    Remove redundant leftmost zeros from opcode bit extraction
    (opcode & 0x0F00) => ((opcode & 0x0F00) >> 8)

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -79,89 +79,89 @@ void Chip8::emulateCycle() {
             pc = opcode & 0x0FFF;
             break;
         case 0x3000: // 0x3XKK: Skip next instr. if V[X] == KK
-            if((V[opcode & 0x0F00] ^ opcode & 0x00FF) == false)
+            if((V[(opcode & 0x0F00) >> 8] ^ opcode & 0x00FF) == false)
                 pc+=2;
             pc+=2;
             break;
         case 0x4000: // 0x4XKK: Skip next instr. if V[X] != KK
-            if(V[opcode & 0x0F00] ^ opcode & 0x00FF)
+            if(V[(opcode & 0x0F00) >> 8] ^ opcode & 0x00FF)
                 pc+=2;
             pc+=2;
             break;
         case 0x5000: // 0x5XY0: Skip next instr. if V[X] == V[Y]
-            if(V[opcode & 0x0F00] ^ V[opcode & 0x00F0])
+            if(V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4])
                  pc+=2;
             pc+=2;
             break;
         case 0x6000: // 0x6XKK: V[X] = KK 
-            V[opcode & 0x0F00] = opcode & 0x00FF;
+            V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
             pc+=2;
             break;
         case 0x7000: // 0x7XKK: V[X] += KK
-            V[opcode & 0x0F00] = V[opcode & 0x0F00] + (opcode & 0x00FF);
+            V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] + (opcode & 0x00FF);
             pc+=2;
             break;
         case 0x8000:
             switch(opcode & 0x000F) {
                 case 0x0000: // 0x8XY0: V[X] = V[Y]
-                    V[opcode & 0x0F00] = V[opcode & 0x00F0];
+                    V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
                     pc+=2;
                     break;
                 case 0x0001: // 0x8XY1: V[X] OR V[Y]
-                    V[opcode & 0x0F00] = V[opcode & 0x0F00] | V[opcode & 0x00F0];
+                    V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] | V[(opcode & 0x00F0) >> 4];
                     pc+=2;
                     break;
                 case 0x0002: // 0x8XY2: V[X] AND V[Y]
-                    V[opcode & 0x0F00] = V[opcode & 0x0F00] & V[opcode & 0x00F0];
+                    V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] & V[(opcode & 0x00F0) >> 4];
                     pc+=2;
                     break;
                 case 0x0003: // 0x8XY3: V[X] XOR V[Y]
-                    V[opcode & 0x0F00] = V[opcode & 0x0F00] ^ V[opcode & 0x00F0];
+                    V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4];
                     pc+=2;
                     break;
                 case 0x0004: // 0x8XY4: V[X] ADD V[Y]
-                    V[opcode & 0x0F00] = (V[opcode & 0x0F00] + V[opcode & 0x00F0]) & 0x00FF; // store only lowest 8 bits
-                    if(V[opcode & 0x0F00] + V[opcode & 0x00F0] > 255)
+                    V[(opcode & 0x0F00) >> 8] = (V[(opcode & 0x0F00) >> 8] + V[(opcode & 0x00F0) > 4]) & 0x00FF; // store only lowest 8 bits
+                    if(V[(opcode & 0x0F00) >> 8] + V[(opcode & 0x00F0) >> 4] > 255)
                         V[0xF] = 1;
                     pc+=2;
                     break;
                 
                 case 0x0005: // 0x8XY5: V[X] SUB V[Y]
-                     V[opcode & 0x0F00] = (V[opcode & 0x0F00] - V[opcode & 0x00F0]) & 0x00FF; // TODO: Store only lowest 8 bits or not?.
-                    if(V[opcode & 0x0F00] > V[opcode & 0x00F0])
+                     V[(opcode & 0x0F00) >> 8] = (V[(opcode & 0x0F00) >> 8] - V[(opcode & 0x00F0) >> 4]) & 0x00FF; // TODO: Store only lowest 8 bits or not?.
+                    if(V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4])
                         V[0xF] = 1;
                     else
                         V[0xF] = 0;
                     pc+=2;
                     break;
                 case 0x0006: // 0x8XY6: V[X] = V[X] / 2 
-                    if(V[opcode & 0x0F00] & 1) // check if last bit is 1
+                    if(V[(opcode & 0x0F00) >> 8] & 1) // check if last bit is 1
                         V[0xF] = 1;
                     else
                         V[0xF] = 0;
-                    V[opcode & 0x0F00] = V[opcode & 0x0F00] >> 1;
+                    V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] >> 1;
                     pc+=2;
                     break;
                 case 0x0007: // 0x8XY7: V[X] SUBN V[Y]
-                     V[opcode & 0x0F00] = (V[opcode & 0x00F0] - V[opcode & 0x0F00]) & 0x00FF;
-                    if(V[opcode & 0x00F0] > V[opcode & 0x0F00])
+                     V[(opcode & 0x0F00) >> 8] = (V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8]) & 0x00FF;
+                    if(V[(opcode & 0x00F0) >> 4] > V[(opcode & 0x0F00) >> 8])
                         V[0xF] = 1;
                     else
                         V[0xF] = 0;
                     pc+=2;
                     break;
                 case 0x000E: // 0x8XYE: V[X] = V[X] * 2 
-                    if(V[opcode & 0x0F00] & 1) // check if last bit is 1
+                    if(V[(opcode & 0x0F00) >> 8] & 1) // check if last bit is 1
                         V[0xF] = 1;
                     else
                         V[0xF] = 0;
-                    V[opcode & 0x0F00] = V[opcode & 0x0F00] << 1;
+                    V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x0F00) >> 8] << 1;
                     pc+=2;
                     break;
             }
             break;
         case 0x9000: // 0x9XY0: Skip next instr. if V[X] != V[Y]
-            if((V[opcode & 0x0F00] ^ V[opcode & 0x00F0]) == false)
+            if((V[(opcode & 0x0F00) >> 8] ^ V[(opcode & 0x00F0) >> 4]) == false)
                  pc+=2;
             pc+=2;
             break;
@@ -193,22 +193,22 @@ void Chip8::emulateCycle() {
         case 0xF000:
             switch(opcode & 0x00FF){
                 case 0x0007: // 0xFX07: V[X] = delay_timer
-                    V[opcode & 0x0F00] = delay_timer;
+                    V[(opcode & 0x0F00) >> 8] = delay_timer;
                     pc+=2;
                     break;
                 case 0x000A: // 0xFX0A: Wait for a key press, store the value of the key in V[X]
                     std::cout << "Not implemented yet!" << std::endl;
                     break;
                 case 0x0015: // 0xFX15: delay_timer = V[X]
-                    delay_timer = V[opcode & 0x0F00];
+                    delay_timer = V[(opcode & 0x0F00) >> 8];
                     pc+=2;
                     break;
                 case 0x0018: // 0xFX18: sound_timer = V[X]
-                    sound_timer = V[opcode & 0x0F00];
+                    sound_timer = V[(opcode & 0x0F00) >> 8];
                     pc+=2;
                     break;
                 case 0x001E: // 0xFX1E: I = I + V[X]
-                    I = I + V[opcode & 0x0F00];
+                    I = I + V[(opcode & 0x0F00) >> 8];
                     pc+=2;
                     break;
                 case 0x0029: // 0xFX29: I = location_of_sprite_for_digit_V[X]
@@ -216,18 +216,18 @@ void Chip8::emulateCycle() {
                     std::cout << "Not implemented yet!" << std::endl;
                     break;
                 case 0x0033: // 0xFX33: Store BCD representation of V[X] in memory locations I, I+1 and I+2
-                    (*memory)[I+2] = V[opcode & 0x0F00] % 10; // ones
-                    (*memory)[I+1] = (V[opcode & 0x0F00] / 10) % 10; // tens
-                    (*memory)[I] = (V[opcode & 0x0F00] / 100) % 10; // hundreds
+                    (*memory)[I+2] = V[(opcode & 0x0F00) >> 8] % 10; // ones
+                    (*memory)[I+1] = (V[(opcode & 0x0F00) >> 8] / 10) % 10; // tens
+                    (*memory)[I] = (V[(opcode & 0x0F00) >> 8] / 100) % 10; // hundreds
                     pc+=2;
                     break;
                 case 0x0055: // 0xFX55: Store registers V[0x0] through V[X] in memory starting at location I
-                    for(unsigned char i = 0x0; i <= (opcode & 0x0F00); ++i)
+                    for(unsigned char i = 0x0; i <= ((opcode & 0x0F00) >> 8); ++i)
                         (*memory)[I] = V[i];
                     pc+=2;
                     break;
-                case 0x0065: // 0xFX55: Store registers V[0x0] through V[X] in memory starting at location I
-                    for(unsigned char i = 0x0; i <= (opcode & 0x0F00); ++i)
+                case 0x0065: // 0xFX65: Read registers V[0x0] through V[X] from memory starting at location I
+                    for(unsigned char i = 0x0; i <= ((opcode & 0x0F00) >> 8); ++i)
                         V[i] = (*memory)[I];
                     pc+=2;
                     break;
